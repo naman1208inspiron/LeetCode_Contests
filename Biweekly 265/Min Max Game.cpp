7a701c80c9a6cd8e4e38efbfb82e9974833346d1
Naman Sharma <74311341+naman1208inspiron@users.noreply.github.com>
2022-06-09 15:59:46 +0530

    Update Min Max Game.cpp

--- Biweekly 265/Min Max Game.cpp
+++ Biweekly 265/Min Max Game.cpp
@@ -13,3 +13,20 @@ public:
         return res;
     }
 };
+//``````````iterative``````````````
+class Solution {
+public:
+    int minMaxGame(vector<int>& nums) {
+        int n= nums.size();
+        while(n!=1){
+            int k=0;
+            for(int i=0; i<n/2; i++){
+                if(k%2==0) nums[k]= min(nums[2*i], nums[2*i+1;
+                else nums[k]= max(nums[2*i], nums[2*i+1]);
+                k++;
+            }
+            n/=2;
+        }
+        return nums[0];
+    }
+};
